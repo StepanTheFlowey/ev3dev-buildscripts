0d3a0f999f5523025d8de840bb75ec0d3960e901
flowey-nuc <stepanmasirov@gmail.com>
2024-11-23 12:02:20 +0500

    speed up repack scripts

--- build-area/repack-all.sh
+++ build-area/repack-all.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 echo "Repacking kernel debs..."
-
-./repack.sh linux-headers-* k-headers.deb
-./repack.sh linux-image-* k-image.deb
+./repack.sh linux-headers-* k-headers.deb &
+./repack.sh linux-image-* k-image.deb &
 ./repack.sh linux-libc-* k-libc.deb
+wait
